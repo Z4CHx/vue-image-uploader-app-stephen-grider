f323d9eac1e6e06714c1d3752970a6fb8522df79
hirodashi <zacharydurland@gmail.com>
2019-01-31 13:00:39 -0600

    add empty header component'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <h3>I is app</h3>
+    <div>
+        <Header></Header>
+    </div>
 </template>
 
 <script>
+import Header from './components/Header'
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+      Header
+  }
 };
 </script>
